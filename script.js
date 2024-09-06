b678341feeb7e8965baab1663c60725c3dbb162e
blaycoder <onatola4real@gmail.com>
2024-09-06 22:28:57 +0100

    added confetti

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ function displayMessage() {
   }
 }
 
+function fireConfetti() {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
+}
+
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(e, btn);
@@ -17,10 +47,9 @@ btn.addEventListener("click", (e) => {
     e.target.innerText = "Close Message 💌";
     bodyContent.style.display = "block";
     displayMessage();
+    fireConfetti();
   } else {
     bodyContent.style.display = "none";
     e.target.innerText = "View Message 💌";
   }
 });
-
-
